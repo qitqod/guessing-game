e67ecfb18903eebfeda7250215318c96ce232030
Ewa Godlewska <ewa.godlewska@m2msuite.com>
2024-11-29 15:33:28 +0100

    Tweak the validation on submit

--- play.py
+++ play.py
@@ -174,15 +174,16 @@ else:
         distance = current_data["distance_km"]
         st.write(f"### Round {st.session_state.round_number}: Guess which capital is {distance} km away from {reference_city}, {reference_country}.")
         user_guess = st.text_input("Enter your guess:").strip()
-        if user_guess and not user_guess.isalpha():
-            st.error("Invalid guess. Please enter a valid string.")
-        elif not user_guess:
-            st.warning("Guess cannot be empty.")
-        else:
-            if st.button("Submit"):
-                evaluate_guess_and_provide_feedback(user_guess)
+
+        if st.button("Submit"):
+            if user_guess and not user_guess.isalpha():
+                st.error("Invalid guess. Please enter a valid string.")
+            elif not user_guess:
+                st.warning("Guess cannot be empty.")
             else:
-                st.warning("Please enter a valid guess!")
+                evaluate_guess_and_provide_feedback(user_guess)
+        else:
+            st.warning("Please enter a valid guess.")
 
     display_tracking_variables()
 
